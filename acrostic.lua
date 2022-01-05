e7b7ee0198134ba2f20ebf7e78a74ab23bf7f47a
Zack Scholl <zack.scholl@gmail.com>
2022-01-05 13:55:17 -0800

    ok

--- acrostic.lua
+++ acrostic.lua
@@ -107,7 +107,7 @@ function key(k,z)
     acrostic:key(k,z)
   elseif global_page==3 then 
     volpan:key(k,z)
-  elseif global_page==4 then
+  elseif global_page==5 then
     monosaw:key(k,d)
   end
 end
@@ -115,7 +115,7 @@ end
 function enc(k,d)
   if global_shift and k==1 then
     -- change global_page
-    global_page=util.clamp(global_page+d,1,4)
+    global_page=util.clamp(global_page+d,1,5)
     if global_page<3 then 
       acrostic:set_page(global_page)
     end
@@ -125,7 +125,13 @@ function enc(k,d)
     acrostic:enc(k,d)
   elseif global_page==3 then 
     volpan:enc(k,d)
-  elseif global_page==4 then
+  elseif global_page==4 then 
+    if k==2 then 
+      params:delta("internote_prob",d)
+    else
+      params:delta("gate_prob",d)
+    end
+  elseif global_page==5 then
     monosaw:enc(k,d)
   end
 end
@@ -146,6 +152,34 @@ function redraw()
   elseif global_page==3 then
     volpan:draw()
   elseif global_page==4 then
+    local a=params:get("gate_prob")
+    local b=params:get("internote_prob")
+    screen.clear()
+    screen.aa(1)
+    local x=0
+    local max=15
+    for i=1,a*max do
+        screen.level(16-math.floor(i))
+        local w=(max-i+1)^2/14+1
+        screen.rect(x,1,w,64)
+        screen.fill()
+        x=x+w+2
+    end
+    screen.update()
+    screen.blend_mode(4)
+    local y=53
+    local max=10
+    for i=1,b*max do
+        screen.level(16-math.floor(i))
+        local h=(max-i+1)^2/14+1
+        screen.rect(1,y,128,h)
+        screen.fill()
+        y=y-h-2
+    end
+    screen.update()
+    screen.blend_mode(0)
+    screen.aa(0)
+  elseif global_page==5 then
     monosaw:draw()
   elseif global_page==0 then 
     monosaw:eyes(startup_eyes.irisSize,startup_eyes.blinkState,startup_eyes.blinkState2,startup_eyes.volume,startup_eyes.brightness)
